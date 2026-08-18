84e941aae851fbb94d438af731d5b2017d23014d
Đạt Nguyễn <dattnguyen1902@gmail.com>
2026-08-18 20:51:42 +0700

    Update B1_Pos_Neg.java

--- B1_Pos_Neg.java
+++ B1_Pos_Neg.java
@@ -8,6 +8,13 @@ public class B1_Pos_Neg {
         Scanner sc = new Scanner(System.in);
         System.out.print("enter a number: ");
         int n = sc.nextInt();
-        System.out.println(n > 0 ? "Positive number" : (n < 0 ? "Nengative number" : "zero"));
+//        System.out.println(n > 0 ? "Positive number" : (n < 0 ? "Nengative number" : "zero"));
+        if (n > 0) {
+            System.out.println("Positive number");
+        } else if (n < 0) {
+            System.out.println("Negative number");
+        } else {
+            System.out.println("Zero");
+        }
     }
 }
